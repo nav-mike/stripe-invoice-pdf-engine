63b8975f79b84e9d115473299adb12fdb1fa0105
Mikhail Navrotskiy <m.navrotskiy@hotmail.com>
2018-05-31 12:29:54 +0000

    fix controller

--- app/controllers/stripe_invoice_pdf_engine/invoices_controller.rb
+++ app/controllers/stripe_invoice_pdf_engine/invoices_controller.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-class InvoicesController < ApplicationController
-  layout 'stripe_invoice_pdf/pdf'
+require_dependency 'stripe_invoice_pdf_engine/application_controller'
 
-  def show
-    @invoice = StripeInvoicePdf::Invoice.new(params[:invoice_id])
-    render pdf: "Invoice-#{@invoice.number}",
-           template: 'invoices/show.pdf.erb',
-           layout: 'stripe_invoice_pdf/pdf.html',
-           show_as_html: show_as_html?
-  end
+module StripeInvoicePdfEngine
+  class InvoicesController < ApplicationController
+    def show
+      @invoice = StripeInvoicePdf::Invoice.new(params[:invoice_id])
+      render pdf: "Invoice-#{@invoice.number}",
+             template: 'invoices/show.pdf.erb',
+             layout: 'stripe_invoice_pdf/pdf.html',
+             show_as_html: show_as_html?
+    end
 
-  private
+    private
 
-  def show_as_html?
-    (Rails.env.development? || Rails.env.test?) && params.key?('debug')
+    def show_as_html?
+      (Rails.env.development? || Rails.env.test?) && params.key?('debug')
+    end
   end
 end

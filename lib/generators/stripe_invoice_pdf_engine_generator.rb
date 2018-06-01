bd2be40a8462fbbc9afae19a06607597d2793321
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-06-01 12:28:46 +0300

    add config file template

--- lib/generators/stripe_invoice_pdf_engine_generator.rb
+++ lib/generators/stripe_invoice_pdf_engine_generator.rb
@@ -2,7 +2,11 @@
 
 if defined?(Rails)
   class StripeInvoicePdfEngineGenerator < Rails::Generators::Base
+    source_root(File.expand_path(File.dirname(__FILE__) + '/../templates'))
+
     def copy_initializer
+      copy_file 'stripe_invoice_pdf_engine.rb', 'config/initializers/stripe_invoice_pdf_engine.rb'
+
       generate 'stripe_invoice_pdf'
     end
   end
---
+++ lib/templates/stripe_invoice_pdf_engine.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# StripeInvoicePdf Global Configuration
+#
+# Use this to set up shared configuration options for your entire application.
+#
+# To learn mode, check out the README:
+#
+# https://github.com/nav-mike/stripe-invoice-pdf/blob/master/README.md
+
+StripeInvoicePdfEngine::Config = {
+  # Define your Stripe api_key:
+  #
+  # api_key: 'sk_...',
+  #
+  # Add your logo here.
+  #
+  # company_logo: 'logo.png',
+  #
+  # Define pdf layout
+  # layout = 'stripe_invoice_pdf_engine/application.html'
+}

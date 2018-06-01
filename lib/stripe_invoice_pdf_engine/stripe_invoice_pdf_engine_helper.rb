da130d41d9e65f495e15ac7773a4d5748e028fff
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-06-01 14:47:03 +0300

    add show as html helper

--- lib/stripe_invoice_pdf_engine/stripe_invoice_pdf_engine_helper.rb
+++ lib/stripe_invoice_pdf_engine/stripe_invoice_pdf_engine_helper.rb
@@ -6,7 +6,11 @@ module StripeInvoicePdfEngine
       render pdf: "Invoice-#{invoice.number}",
              template: 'stripe_invoice_pdf_engine/invoices/show.pdf.erb',
              layout: 'stripe_invoice_pdf_engine/application.html',
-             show_as_html: true
+             show_as_html: show_as_html?
+    end
+
+    def show_as_html?
+      (Rails.env.development? || Rails.env.test?) && params.key?('dedbug')
     end
   end
 end

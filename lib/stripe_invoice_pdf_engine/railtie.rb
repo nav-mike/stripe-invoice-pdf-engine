dc8d906d9c5ebe91ff20b25ea3b99dd319d3c8ea
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-10-04 16:38:12 +0300

    #12 fix mount error

--- lib/stripe_invoice_pdf_engine/railtie.rb
+++ lib/stripe_invoice_pdf_engine/railtie.rb
@@ -11,7 +11,7 @@ module StripeInvoicePdfEngine
                                       StripeInvoicePdfEngine::StripeInvoicePdfEngineHelper
         end
       end
-    elsif RAILS::VERSION::MAJOR == 4
+    elsif Rails::VERSION::MAJOR == 4
       class StripeInvoicePdfEngineRailtie < Rails::Railtie
         initializer 'stripe_invoice_pdf_engine.register' do |_app|
           ActionController::Base.send :prepend,

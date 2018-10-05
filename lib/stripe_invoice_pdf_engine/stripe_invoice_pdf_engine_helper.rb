7f98fddff07199bbdd7f59d2170bcc0a1f233871
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-10-05 13:37:33 +0300

    #12 add logo helpers

--- lib/stripe_invoice_pdf_engine/stripe_invoice_pdf_engine_helper.rb
+++ lib/stripe_invoice_pdf_engine/stripe_invoice_pdf_engine_helper.rb
@@ -12,5 +12,23 @@ module StripeInvoicePdfEngine
     def show_as_html?
       (Rails.env.development? || Rails.env.test?) && params.key?('dedbug')
     end
+
+    def invoice_logo_tag
+      return yield if block_given?
+      name = StripeInvoicePdfEngine.config.company_logo
+      exts = %w[.jpg .png .jpeg]
+      return image_logo(name) if exts.include?(File.extname(name))
+      logo_svg name
+    end
+
+    def image_logo(name)
+      image_tag name
+    end
+
+    def logo_svg(name = nil)
+      return logo_svg_file(name) if name.present?
+      raise 'Insert SVG here!' unless block_given?
+      yield
+    end
   end
 end

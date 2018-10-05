ee196622aebad031b09cce7eba10f9256000ce3e
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-10-05 12:06:28 +0300

    #12 use correct model

--- app/controllers/stripe_invoice_pdf_engine/invoices_controller.rb
+++ app/controllers/stripe_invoice_pdf_engine/invoices_controller.rb
@@ -5,7 +5,7 @@ require_dependency 'stripe_invoice_pdf_engine/application_controller'
 module StripeInvoicePdfEngine
   class InvoicesController < ApplicationController
     def show
-      @invoice = StripeInvoicePdf::Invoice.new(params[:invoice_id])
+      @invoice = StripeInvoicePdfEngine::Invoice.new(params[:invoice_id])
       render_invoice @invoice
     end
   end

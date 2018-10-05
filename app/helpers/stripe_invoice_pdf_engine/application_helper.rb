8e1f09b2ca0e71264d289efbbf822b6f5e8fd0c3
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2018-10-05 16:18:11 +0300

    #12 fix view helpers

--- app/helpers/stripe_invoice_pdf_engine/application_helper.rb
+++ app/helpers/stripe_invoice_pdf_engine/application_helper.rb
@@ -2,5 +2,22 @@
 
 module StripeInvoicePdfEngine
   module ApplicationHelper
+    def invoice_logo_tag
+      return yield if block_given?
+      name = StripeInvoicePdfEngine.config.company_logo
+      exts = %w[.jpg .png .jpeg]
+      return image_logo(name) if exts.include?(File.extname(name))
+      logo_svg name
+    end
+
+    def image_logo(name)
+      image_tag name
+    end
+
+    def logo_svg(name = nil)
+      return logo_svg_file(name) if name.present?
+      raise 'Insert SVG here!' unless block_given?
+      yield
+    end
   end
 end
